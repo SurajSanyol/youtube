10e36b752aa6d8c5c49bf8186f2ac9d843e26ce0
SurajSanyol <84567968+SurajSanyol@users.noreply.github.com>
2024-06-19 14:34:46 +0530

    Update constant.js remove comment

--- src/utills/constant.js
+++ src/utills/constant.js
@@ -1,45 +1,12 @@
 
 
 const YOUR_API_KEY = "AIzaSyB4KD8JmuwmnUMi8OFXBYA5dHuAY9daWTw";
-// AIzaSyB4KD8JmuwmnUMi8OFXBYA5dHuAY9daWTw
-// AIzaSyCPtVDzFdeeikIN_AMpk2gZ1i4SiI8tRuw
-// AIzaSyB4KD8JmuwmnUMi8OFXBYA5dHuAY9daWTw
-// AIzaSyDKCANSyQvLFz0tb62tcC7kYepWBnOm4FA
-
-// rasheed Api   AIzaSyDH5y2bnrlTXe-mnE7ZUpRERUzZftaily8
-
  export const YOUTUBE_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+YOUR_API_KEY; 
-
-
-//  'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=AIzaSyB4KD8JmuwmnUMi8OFXBYA5dHuAY9daWTw' ;
-
-// https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=
-
-
 export const channelDetails1= "https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id="
-
-// UC_x5XG1OV2P6uZZ5FSM9Ttw
-
 export const channelDetails2="&key="+YOUR_API_KEY;
-
-
-// https://yt3.googleusercontent.com/sZ7ML82fcVAyW7FlaFcbBiRQSDeY-rjfwWB7khgGXRXBYnyk6teEzUqIYxx-2YZduT2b4uOP=s160-c-k-c0x00ffffff-no-rj
-
-// https://yt3.googleusercontent.com/b9mNrsonA2WHfFuE3AtKaXETdpWQ2z3evh2X6hg_V1jN13NDrqKQuskQRmVthsO9NfxucOW0wA=s160-c-k-c0x00ffffff-no-rj
-
-
 export const YOUTUBE_SUGG_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
-
-
 export const VIDEO_COMMENT_API1 ='https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId='
-
-
 export const VIDEO_COMMENT_API2 ='&key='+YOUR_API_KEY;
-
 export const SEARCH_API1 = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q='
-
 export const SEARCH_API2=  'surfing&key='+ YOUR_API_KEY;
-
-// https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=AIzaSyB4KD8JmuwmnUMi8OFXBYA5dHuAY9daWTw
-
 export const LIVE_CHAT_COUNT = 50;
