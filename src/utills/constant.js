03c9ca1dfeaf480148886f3288bef96002b0960f
SurajSanyol <84567968+SurajSanyol@users.noreply.github.com>
2024-06-22 15:57:20 +0530

    Update constant.js hiding the keys from constant.js file

--- src/utills/constant.js
+++ src/utills/constant.js
@@ -1,12 +1,12 @@
 
 
-const YOUR_API_KEY = "AIzaSyB4KD8JmuwmnUMi8OFXBYA5dHuAY9daWTw";
- export const YOUTUBE_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+YOUR_API_KEY; 
-export const channelDetails1= "https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id="
-export const channelDetails2="&key="+YOUR_API_KEY;
-export const YOUTUBE_SUGG_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
-export const VIDEO_COMMENT_API1 ='https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId='
-export const VIDEO_COMMENT_API2 ='&key='+YOUR_API_KEY;
-export const SEARCH_API1 = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q='
+const YOUR_API_KEY = 
+ export const YOUTUBE_API = 
+export const channelDetails1= 
+export const channelDetails2=
+export const YOUTUBE_SUGG_API = 
+export const VIDEO_COMMENT_API1 =
+export const VIDEO_COMMENT_API2 =
+export const SEARCH_API1 = 
 export const SEARCH_API2=  'surfing&key='+ YOUR_API_KEY;
 export const LIVE_CHAT_COUNT = 50;
